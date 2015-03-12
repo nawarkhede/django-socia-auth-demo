08729155477276635794b701bb1c963f8af30113
Nishant <Nishant.nawarkhede@gmail.com>
2015-03-12 08:30:56 +0530

    created sample project with social login

---
+++ testapp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'testapp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+	url('', include('social.apps.django_app.urls', namespace='social')),
+	url('', include('django.contrib.auth.urls', namespace='auth')),
+	url(r'^$', 'testapp.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+)
